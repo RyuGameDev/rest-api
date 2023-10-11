8c6620ec1a96b3715f4a8b129e579e88070140a3
Ryujin <127178263+RyuGameDev@users.noreply.github.com>
2023-10-11 14:26:06 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+global.creator = 'Ryudev'// yourname
+global.MONGO_DB_URI = "mongodb+srv://ryudevid:Cahngoding1@cluster0.xknn0zv.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "ryudev.id@gmail.com" //email
+global.email_password = "ibai besd diej duen" //application password email
+global.limitCount = 15
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
